8bda559f9694082527179ea9636417b13dc480bc
TheNoblestman <TheNoblestman@users.noreply.github.com>
2016-12-07 11:58:17 -0500

    MoveList: Add new moves & remove serializable (#16)

--- MoveList.java
+++ MoveList.java
@@ -1,9 +1,26 @@
-public enum MoveList implements java.io.Serializable
+public enum MoveList
 {
     UP (0,1),
+    UP_RIGHT(1, 1),
+    RIGHT(1, 0),
+    DOWN_RIGHT(1, -1),
     DOWN(0,-1),
+    DOWN_LEFT(-1, -1),
     LEFT(-1, 0),
-    RIGHT(1, 0);
+    UP_LEFT(-1, 1),
+    
+    KNIGHT_LEFT_UP(-2, 1),
+    KNIGHT_UP_LEFT(-1, 2),
+    KNIGHT_UP_RIGHT(1, 2),
+    KNIGHT_RIGHT_UP(2, 1),
+    
+    KNIGHT_RIGHT_DOWN(2, -1),
+    KNIGHT_DOWN_RIGHT(1, -2),
+    KNIGHT_DOWN_LEFT(-1, -2),
+    KNIGHT_LEFT_DOWN(-2, -1),
+    
+    DOUBLE_UP(0, 2),
+    DOUBLE_DOWN(0, -2);
 
     private int x;
     private int y;
