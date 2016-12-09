8989d1fe3991bbe15fbc7c0c91e897cbefca0d14
Steven Vascellaro <S.Vascellaro@gmail.com>
2016-12-09 11:25:40 -0500

    Cleanup extra code (#19)
    Space:
    -Simplify isOccupied()
    -Make global variables private
    Piece:
    -Remove extra spacing

--- Piece.java
+++ Piece.java
@@ -3,36 +3,36 @@ import javafx.scene.image.*;
 public class Piece
 {
     //to be made abstract later, with getValidMoves() an abstract method
-    
+
     protected String name;
     protected Image image;
     protected boolean color;
     protected boolean singleMove;
-    
+
     // validMoves
-    
+
     public Piece(String name, boolean color)
     {
         this.name = name;
         this.color = color;
-        
+
         //since image can be inferred from name, probably won't be needed once have Piece subclasses
         String location = "assets/pieces/";
         String filename = this.getColor() + "_" + this.getName() + ".png";
         this.image = new Image(location + filename);
     }
-    
+
     public String getName()
     {
         return this.name;
     }
-    
+
     // Returns image of chess piece
     public Image getImage()
     {
         return this.image;
     }
-    
+
     // Get piece color as string
     public String getColor()
     {
@@ -41,13 +41,13 @@ public class Piece
         else
             return "black";
     }
-    
+
     // returns true if color is white
     public boolean isWhite()
     {
         return this.color;
     }
-    
+
     public String toString()
     {
         return (this.getName() + " " + this.getColor());
